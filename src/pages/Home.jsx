9d429d40fe0a9fb1067c7dcf30c6886bcb7bd976
Rafael Munetiko <munetiko96@gmail.com>
2025-04-24 16:52:19 -0300

    moviecard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,30 @@ const Home = () => {
 
     const getTopRatedMovies = async (url) => {
 
-        const res = await fetch(url)
-        const data = await res.json()
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
     }
 
+    useEffect (() => {
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
 
-    return <div>Home</div>
-    }
+
+        getTopRatedMovies(topRatedUrl);
+    }, []);
+
+
+    return (
+    <div className="container">
+        <h2 className="title">Melhores filmes:</h2>
+        <div className="movies-container">
+            {topMovies.length === 0 && <p>Carregando...</p>}
+            {topMovies.length > 0 && topMovies.map((movie)=> <MovieCard key={movie.id} movie={movie} />)}
+        </div>
+
+    </div>
+    );
+};
 
 export default Home
