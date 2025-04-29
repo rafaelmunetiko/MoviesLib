10033b719a09dc3bc42b8fa9c4a6efe5d95706af
Rafael Munetiko <munetiko96@gmail.com>
2025-04-29 15:40:32 -0300

    Finalizado

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -25,6 +25,13 @@ const Movie = () => {
         setMovie(data);
     }
 
+    const formatCurrency = (number) => {
+        return number.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+        });
+    };
+
     useEffect(() => {
         const movieUrl =`${moviesURL}${id}?${apiKey}`
         getMovie(movieUrl)
@@ -37,9 +44,25 @@ const Movie = () => {
                 <p className="tagline">{movie.tagline}</p>
                 <div className="info">
                     <h3>
-                        <BsWallet2 /> Orçamento
+                        <BsWallet2 /> Orçamento:
                     </h3>
-                    <p>{movie.budget}</p>
+                    <p>{formatCurrency(movie.budget)}</p>
+                </div>
+                <div className="info">
+                    <h3>
+                        <BsGraphUp /> Receita:
+                    </h3>
+                    <p>{formatCurrency(movie.revenue)}</p>
+                </div><div className="info">
+                    <h3>
+                        <BsHourglassSplit /> Duração:
+                    </h3>
+                    <p>{movie.runtime} minutos</p>
+                </div><div className="info description">
+                    <h3>
+                        <BsFillFileEarmarkTextFill /> Descrição:
+                    </h3>
+                    <p>{movie.overview}</p>
                 </div>
             </>
         )}
