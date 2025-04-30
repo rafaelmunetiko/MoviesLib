51e278fa3a822bfac6839fce3dd2ee17ba5e08e8
Rafael Munetiko <munetiko96@gmail.com>
2025-04-30 15:14:46 -0300

    Correções

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   return (
     <nav id="navbar">
           <h2>
-            <Link to="/"><BiCameraMovie/>MoviesLib</Link>
+            <Link to="/"><BiCameraMovie/>SlateScope</Link>
           </h2>
         <form onSubmit={handleSubmit}>
             <input type="text" placeholder='Busque um filme' onChange={(e) => setSearch(e.target.value)} value={search} />
